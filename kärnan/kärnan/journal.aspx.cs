ab02c5c7412e1e5cb387a1e97ad7b0c0bd87a2f9
JennieLindqvist <jennie.lindqvist@hotmail.se>
2017-04-12 15:24:43 +0200

    Front-end Journal

---
+++ kärnan/kärnan/journal.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace kärnan
+{
+    public partial class journal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            drpEnhet.Items.Add("Blå");
+            drpEnhet.Items.Add("Grön");
+            drpEnhet.Items.Add("Gul");
+
+            drpKlient.Items.Add("Mamma");
+            drpKlient.Items.Add("Pappa");
+            drpKlient.Items.Add("Barn");
+
+            
+        }
+    }
+}
